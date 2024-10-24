b507c43e5c58d48b0ccb52e248e1f69a7bad6264
mousam <mousampandey61@gmail.com>
2024-10-24 18:21:56 +0530

    ""

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo " BUILD START"
 python3 -m venv env
-source .env/bin/activate
+source ./env/bin/activate
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
